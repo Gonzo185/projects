f2885ade15ec7d144fd990a8fab88d0d94fd472d
Mousebait <lmgonzalez50023@gmail.com>
2025-04-04 21:12:53 +0000

    Made changes to use python file from a different dictory as module and grab the device list from there

--- ssh_auto/netmiko_automation_from_file.py
+++ ssh_auto/netmiko_automation_from_file.py
@@ -1,49 +1,15 @@
 #!/usr/bin/env python3
 
 from netmiko import ConnectHandler
+import importlib.util
+import sys
 
-#host = input("Please enter hostnam or IP address: ")
-user = "Gonzo185"
-password = "Gonzo185!"
+#Create module to use device list
+spec = importlib.util.spec_from_file_location("CML_devices", "/home/gonzo185/Documents/Lab_Inventory/CML_devices.py")
+CML_devices = importlib.util.module_from_spec(spec)
+sys.modules["CML_devices"] = CML_devices
+spec.loader.exec_module(CML_devices)
 
-R1 = {
-            "device_type":"cisco_ios",
-                "host":"172.16.0.1",
-                    "username":user,
-                        "password":password,
-                        }
-R2 = {
-            "device_type":"cisco_ios",
-                "host":"10.0.0.2",
-                    "username":user,
-                        "password":password,
-                        }
-R3 = {
-            "device_type":"cisco_ios",
-                "host":"10.0.5.3",
-                    "username":user,
-                        "password":password,
-                        }
-R4 = {
-            "device_type":"cisco_ios",
-                "host":"10.0.6.4",
-                    "username":user,
-                        "password":password,
-                        }
-R5 = {
-            "device_type":"cisco_ios",
-                "host":"10.0.5.5",
-                    "username":user,
-                        "password":password,
-                        }
-R6 = {
-            "device_type":"cisco_ios",
-                "host":"10.0.6.6",
-                    "username":user,
-                        "password":password,
-                        }
-
-device_list = [R1, R2, R3, R4, R5, R6]
 config_list = []
 
 with open("router_configs", "r") as file:
@@ -51,7 +17,7 @@ with open("router_configs", "r") as file:
     for line in contents:
         config_list.append(line)
 
-for device in device_list:
+for device in CML_devices.device_list:
     net_connect = ConnectHandler(**device)
     output = net_connect.send_config_set(config_list)
 
